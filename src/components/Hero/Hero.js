dfc4c95023550c1cd99819bc566eda02b4e6b406
Puchana <puchana123@gmail.com>
2023-04-21 15:04:35 +0700

    UI-Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br/>
+        My Personal Porfolio
+      </SectionTitle>
+      <SectionText>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eaque quasi aut vitae qui laudantium est doloribus provident reiciendis tempore ad impedit quibusdam quam accusamus nulla quidem, exercitationem earum dignissimos?
+      </SectionText>
+      <Button onClick={()=>window.location='https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
