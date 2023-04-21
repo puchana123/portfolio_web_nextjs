e6a6c012ba49af452b3e9949f769f1f75060eb69
Puchana <puchana123@gmail.com>
2023-04-21 14:56:58 +0700

    UI-header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,42 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons,Span } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href='/'>
+        <p style={{display:'flex',alignItems:'center',color:'white'}}>
+          <DiCssdeck size='4rem'/><Span>Portfolio</Span>
+        </p>
+      </Link>
+    </Div1>
+
+    <Div2>
+      <li>
+        <NavLink href="#projects">Projects</NavLink>
+      </li>
+      <li>
+        <NavLink href="#tech">Technology</NavLink>
+      </li>
+      <li>
+        <NavLink href="#about">About</NavLink>
+      </li>
+    </Div2>
+
+    <Div3>
+      <SocialIcons href='https://github.com'>
+        <AiFillGithub size='3rem' />
+      </SocialIcons>
+      <SocialIcons href='https://linkedin.com'>
+        <AiFillLinkedin size='3rem' />
+      </SocialIcons>
+      <SocialIcons href='https://instagram.com'>
+        <AiFillInstagram size='3rem' />
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
